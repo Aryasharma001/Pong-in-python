633c7cb13e6da000a27c1fc9735be6cd174c6a2d
Arya Sharma <98249716+Aryasharma001@users.noreply.github.com>
2022-07-24 00:21:12 +0530

    Create paddle_b.py

---
+++ paddle_b.py
@@ -0,0 +1,8 @@
+# Paddle B
+paddle_b = turtle.Turtle()
+paddle_b.speed(0)
+paddle_b.shape("square")
+paddle_b.color("white")
+paddle_b.shapesize(stretch_wid=5,stretch_len=1)
+paddle_b.penup()
+paddle_b.goto(350, 0)
